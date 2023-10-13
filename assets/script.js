43c7287d986025950711c72c86e8173081a2fac2
tim <timour1712@gmail.com>
2023-10-13 11:29:17 +0200

    added slider arrow work

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,97 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+var current_slide_num = 0;
+
+var image = document.getElementsByClassName("banner-img");
+
+
+const myRightButton = document.getElementsByClassName("arrow_right");
+
+Array.from(myRightButton).forEach(Rb => {
+	Rb.addEventListener("click",
+		function () {
+			var banner = document.querySelectorAll("#banner p");
+
+			current_slide_num += 1;
+			if (current_slide_num > 3) {
+				current_slide_num = 0;
+
+			}
+			unselect_dot(sub_dot);
+			// dots[current_slide_num].classList.add("dot_selected");
+			document.getElementsByClassName('dot')[current_slide_num].classList.add("dot_selected");
+
+			image[0].src = "./assets/images/slideshow/" + slides[current_slide_num].image;
+
+			var element = document.createElement("p");
+
+			banner[0].innerHTML = slides[current_slide_num].tagLine;
+			console.log(banner);
+
+			//document.getElementById("banner").replaceChild(element, banner[0]);
+			// const parentDiv = sp2;
+			// parentDiv.replaceChild(banner, p);
+		});
+});
+
+const myLeftButton = document.getElementsByClassName("arrow_left");
+
+Array.from(myLeftButton).forEach(Rb => {
+	Rb.addEventListener("click", function() {
+		var banner = document.querySelectorAll("#banner p");
+
+		current_slide_num -= 1;
+		if (current_slide_num < 0 ) {
+			current_slide_num = 3;
+		}
+		unselect_dot(sub_dot);
+		document.getElementsByClassName('dot')[current_slide_num].classList.add("dot_selected");
+
+		image[0].src = "./assets/images/slideshow/" + slides[current_slide_num].image;
+
+		var element = document.createElement("p");
+
+		banner[0].innerHTML = slides[current_slide_num].tagLine;
+	});
+});
+
+const dots = document.getElementsByClassName("dots");
+
+function afficherBoutonsDesSlides(slides) {
+
+	for (const slide of slides) {
+		var element = document.createElement("div");
+		// remplacer/modifier à la source de l'img
+		element.className = "dot";
+		// inserer dans le html
+		dots[0].appendChild(element).addEventListener("click", function() {
+			// unselect_dot(sub_dot);
+			// dot.classList.add("dot_selected");
+			image[0].src = "./assets/images/slideshow/" + slide.image;
+
+		});
+
+	}
+}
+
+afficherBoutonsDesSlides(slides);
+
+
+function unselect_dot(dots) {
+	Array.from(dots).forEach(dot => {
+		dot.classList.remove("dot_selected");
+	});
+}
+
+
+const sub_dot = document.getElementsByClassName("dot");
+sub_dot[0].classList.add("dot_selected");
+
+Array.from(sub_dot).forEach(dot => {
+	dot.addEventListener("click", function() {
+		unselect_dot(sub_dot);
+		dot.classList.add("dot_selected");
+	});
+});
+
